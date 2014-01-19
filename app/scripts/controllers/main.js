17c2182783c779dd286896bf5de6f74784a0b4dc
Jory Graham <jory.graham@gmail.com>
2014-01-18 21:26:30 -0500

    Just some buttons! And it buzzes!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('vibratileApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $window) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $window.navigator.vibrate(1000);
   });
